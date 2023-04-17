5d66df15600f78eaaab35623f9ea993741cb17d3
Siddharth Desai <siddharth.desai1996@gmail.com>
2023-04-17 18:25:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>Facebook Clone React App</h1>
+   <h2>-By yours truly</h2>
+   <h2>Siddharth Desai </h2>
     </div>
   );
 }
